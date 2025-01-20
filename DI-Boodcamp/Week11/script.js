21095177b41b7b0ad2a383b0d7587c64011e1b4a
ArtemGalkoff <aegalkov@gmail.com>
2025-01-20 20:27:03 +0200

    push

---
+++ DI-Boodcamp/Week11/script.js
@@ -0,0 +1,39 @@
+/* const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+let JSONmarioGame = JSON.stringify(marioGame, null, 2); */
+
+
+
+Examples
+
+/* allTruthy(true, true, true) ➞ true
+
+allTruthy(true, false, true) ➞ false
+
+allTruthy(5, 4, 3, 2, 1, 0) ➞ false */
+
+function allTruthy(...args) {
+    return args.every(Boolean);
+  }
